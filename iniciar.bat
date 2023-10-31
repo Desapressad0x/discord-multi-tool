8b1b8315179be42c4df17e6b5aacc54cb200b39b
неторопливый <59629875+Desapressad0x@users.noreply.github.com>
2023-10-31 15:08:30 -0300

    Update iniciar.bat

--- iniciar.bat
+++ iniciar.bat
@@ -1,7 +1,7 @@
 @echo off
 if exist "node_modules" (
-  start cmd /k "cd %~dp0 && node index.js"
+  start cmd /k "cd %~dp0 && node discord-multi-tool.js"
 ) else (
   npm i
-  start cmd /k "cd %~dp0 && node index.js"
+  start cmd /k "cd %~dp0 && node discord-multi-tool.js"
 )
