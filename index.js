1443462c4c6843a304d129285b3833c44486666d
неторопливый <59629875+Desapressad0x@users.noreply.github.com>
2023-11-09 17:30:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ Discord multi-tool 2.0 by Desapressado (2023)
 
 Uso: node ${path.basename(__filename)} [-d delay] [--remover-amigos] [-t token] [-i id]
 
-  -d                Personaliza o delay de remoção em segundos (padrão: 1)
+  -d                Personaliza o delay de uma ação em segundos (padrão: 1)
   -t                Token de autorização da sua conta Discord
   -i                ID para operação específica
   --remover-amigos  Remove todos os amigos da sua conta
