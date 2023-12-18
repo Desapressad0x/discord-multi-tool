94708fc91dc1a988d9624963fb8d6ad611b8ac6a
неторопливый <59629875+Desapressad0x@users.noreply.github.com>
2023-12-18 14:25:30 -0300

    Delete src/iniciar.sh

--- src/iniciar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -d "../node_modules" ]; then
-  gnome-terminal --working-directory="$(pwd)" -- bash -c "node index.js; exec bash"
-else
-  npm install
-  gnome-terminal --working-directory="$(pwd)" -- bash -c "node index.js; exec bash"
-fi
